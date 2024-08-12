b7544f6fcacf95c9f911a690fcfedba6198c6bcd
Marc Zweiler <marc.zweiler@outlook.at>
2024-08-13 00:18:29 +0200

    Added more Token to the Token enum Created function which returns the regex for the specified token

--- src/lexer.rs
+++ src/lexer.rs
@@ -6,18 +6,65 @@ use regex::Regex;
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Token {
-    Def, // function definitions
-    Use, // import keywords
-    Delimiter, // the semicolon
-    Indentation, // Indentation
-    LineBreak, // Line break
-    OpeningParenthesis,
-    ClosingParenthesis,
-    Comma,
+    // Keywords
+    Def, Pub, Own, Use, For, While, Do, In, If, Else, Enum,
+
+    // Symbols
+    Colon, Comma, Delim, Equals, Arrow, OpenPar, ClosePar,
+
+    // Formatting
+    Indent, LineBr,
+
+    // " and '
+    Quote, CharSign,
+
+    // Variable types
     Ident(String),
     Number(i32),
     Operator(String)
 }
+
+impl Token {
+    pub fn regex(&self) -> Regex {
+        match &self {
+            // Keywords
+            Token::Def => Regex::new(r"(?P<def>def)|").unwrap(),
+            Token::Pub => Regex::new(r"(?P<pub>pub)|").unwrap(),
+            Token::Own => Regex::new(r"(?P<own>own)|").unwrap(),
+            Token::Use => Regex::new(r"(?P<use>use)|").unwrap(),
+            Token::For => Regex::new(r"(?P<for>for)|").unwrap(),
+            Token::While => Regex::new(r"(?P<while>while)|").unwrap(),
+            Token::Do => Regex::new(r"(?P<do>do)|").unwrap(),
+            Token::In => Regex::new(r"(?P<in>in)|").unwrap(),
+            Token::If => Regex::new(r"(?P<if>if)|").unwrap(),
+            Token::Else => Regex::new(r"(?P<else>else)|").unwrap(),
+            Token::Enum => Regex::new(r"(?P<enum>enum)|").unwrap(),
+
+            // Symbols
+            Token::Colon => Regex::new(r"(?P<colon>:)|").unwrap(),
+            Token::Comma => Regex::new(r"(?P<comma>,)|").unwrap(),
+            Token::Delim => Regex::new(r"(?P<delim>;)|").unwrap(),
+            Token::Equals => Regex::new(r"(?P<equals>=)<").unwrap(),
+            Token::Arrow => Regex::new(r"(?P<arrow>->)|").unwrap(),
+            Token::OpenPar => Regex::new(r"(?P<openPar>\())|").unwrap(),
+            Token::ClosePar => Regex::new(r"(?P<closePar>\))|").unwrap(),
+            
+            // Formatting
+            Token::Indent => Regex::new(r"(?P<indent>\t)|").unwrap(),
+            Token::LineBr => Regex::new(r"(?P<lineBr>\n)|").unwrap(),
+
+            // Text
+            Token::Quote => Regex::new(r"(?P<quote>)|").unwrap(),
+            Token::CharSign => Regex::new(r"(?P<charSign>')|").unwrap(),
+
+            // Variables
+            Token::Ident(_) => Regex::new(r"(?P<ident>\p{Alphabetic}\w*)|").unwrap(),
+            Token::Number(_) => Regex::new(r"(?P<number>\d+\.?\d*)|").unwrap(),
+            Token::Operator(_) => Regex::new(r"(?P<operator>\S)|").unwrap() 
+        }
+    }
+}
+
 
 pub struct Lexer {
 }
@@ -74,17 +121,17 @@ impl Lexer {
                     Err(_) => panic!("Lexer failed trying to parse number")
                 }
             } else if cap.name("delimiter").is_some() {
-                Token::Delimiter
+                Token::Delim
             } else if cap.name("oppar").is_some() {
-                Token::OpeningParenthesis
+                Token::OpenPar
             } else if cap.name("clpar").is_some() {
-                Token::ClosingParenthesis
+                Token::ClosePar
             } else if cap.name("comma").is_some() {
                 Token::Comma
             } else if cap.name("indent").is_some() {
-                Token::Indentation
+                Token::Indent
             } else if cap.name("linebr").is_some() {
-                Token::LineBreak
+                Token::LineBr
             } else {
                 Token::Operator(cap.name("operator").unwrap().as_str().to_string())
             };
