6e42dd340979566d68da50f34726490c14cd2331
Marc Zweiler <marc.zweiler@outlook.at>
2024-08-12 16:03:50 +0200

    Pushed changes in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 use crate::repl::Repl;
+use crate::lexer::Lexer;
+use crate::parser::Parser;
 
 mod scanner;
 mod repl;
+mod lexer;
+mod parser;
 
 fn main() {
     let mut repl = Repl::new();
+    let lexer = Lexer::new();
+    let parser = Parser::new();
     let args: Vec<String> = std::env::args().collect();
 
     if args.len() > 2 {
         println!("Usage: flint [source file]");
         std::process::exit(64);
     } else if args.len() == 2 {
-        repl.run_file(&args[1]);
+        //repl.run_file(&args[1]);
+        let tokens = lexer.tokenize_file(&args[1]);
+        parser.parse_tokens(tokens);
     } else {
         repl.run_prompt();
     }
