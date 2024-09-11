4384e0360f1bdda1dfff5108a353f29ec2f665c7
Marc Zweiler <marc.zweiler@outlook.at>
2024-09-11 20:49:46 +0200

    Removed all rust llvm work we have done so far, C++ backend will come soon

--- src/lib.rs
+++
@@ -1,8 +0,0 @@
-mod scanner;
-mod repl;
-mod token;
-
-pub use repl::Repl;
-pub use scanner::Scanner;
-pub use token::Token;
-pub use token::TokenType;
--- tests/integration_test.rs
+++
@@ -1,7 +0,0 @@
-use flint::Repl;
-
-#[test]
-fn test_repl_initialization() {
-    let repl = Repl::new();
-    // Test implementation
-}
