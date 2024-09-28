24d4a77acfc3dcd593c96365c22e2bc8410b2cc1
Marc Zweiler <marc.zweiler@outlook.at>
2024-09-28 21:01:44 +0200

    Added shell.nix file to declaratively setup the dev-environment inside NixOS

---
+++ shell.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05") {} }:
+
+pkgs.mkShellNoCC {
+	packages = with pkgs; [
+		llvm_18
+		cmake
+		gcc
+		gpp
+	];
+	shellHook = ''
+		export DEV_ENVIRONMENT="flint"
+		export NIX_SHELL_PATH="$(pwd)"
+		source ~/.bashrc
+		alias build-flint="$NIX_SHELL_PATH/flint/"
+		alias build-calculator="$NIX_SHELL_PATH/resources/flint/calculator/build.sh"
+		alias run-calculator="$NIX_SHELL_PATH/resources/flint/calculator/calculator"
+		alias build-tutorial="$NIX_SHELL_PATH/resources/tutorial/kaleidoscope/src/build.sh"
+		alias run-tutorial="$NIX_SHELL_PATH/resources/tutorial/kaleidoscope/output/main"
+	'';
+}
